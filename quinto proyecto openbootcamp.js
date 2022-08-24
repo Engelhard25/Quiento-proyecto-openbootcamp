e58bd50c50fb9307d5dac3e55b46637db56cd428
engelhard <engelhard25@hotmail.com>
2022-08-24 16:18:30 -0400

    Quinto proyecto openbootcamp

---
+++ quinto proyecto openbootcamp.js
@@ -0,0 +1,13 @@
+let altura_cm = 197;
+let altura_mt = 1.97;
+let peso_kg = 92.7;
+let altura_mt_up = Math.ceil(altura_mt);
+let peso_kg_down = Math.floor(peso_kg);
+let igualdad_valores = Number.MAX_VALUE === Number.MAX_VALUE +1;
+
+console.log(altura_cm);
+console.log(altura_mt);
+console.log(peso_kg);
+console.log(altura_mt_up);
+console.log(peso_kg_down);
+console.log(igualdad_valores);
